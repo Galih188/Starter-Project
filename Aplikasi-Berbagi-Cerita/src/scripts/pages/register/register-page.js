fd985378bf86c8bc3b8d4e44043f4914f6865c20
Galihhh <127461042+Galih188@users.noreply.github.com>
2025-05-12 23:37:08 +0700

    Update register-page.js

--- Aplikasi-Berbagi-Cerita/src/scripts/pages/register/register-page.js
+++ Aplikasi-Berbagi-Cerita/src/scripts/pages/register/register-page.js
@@ -1,55 +1,22 @@
+import RegisterView from "./register-view.js";
+import RegisterPresenter from "./register-presenter.js";
+import RegisterModel from "./register-model.js";
+
 export default class RegisterPage {
+  #view;
+  #presenter;
+
+  constructor() {
+    this.#view = new RegisterView();
+    const model = new RegisterModel();
+    this.#presenter = new RegisterPresenter({ model, view: this.#view });
+  }
+
   async render() {
-    return `
-        <section class="container">
-          <h1>Daftar</h1>
-          <form id="register-form">
-            <label for="name">Nama:</label>
-            <input type="text" id="name" required />
-  
-            <label for="email">Email:</label>
-            <input type="email" id="email" required />
-  
-            <label for="password">Password:</label>
-            <input type="password" id="password" required minlength="8" />
-  
-            <button type="submit">Daftar</button>
-          </form>
-        </section>
-      `;
+    return this.#view.getTemplate();
   }
 
   async afterRender() {
-    const form = document.querySelector("#register-form");
-    form.addEventListener("submit", async (e) => {
-      e.preventDefault();
-
-      const name = document.getElementById("name").value;
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-
-      try {
-        const response = await fetch(
-          "https://story-api.dicoding.dev/v1/register",
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ name, email, password }),
-          }
-        );
-
-        const result = await response.json();
-
-        if (!result.error) {
-          alert("Registrasi berhasil! Silakan login.");
-          window.location.hash = "/login";
-        } else {
-          alert(result.message);
-        }
-      } catch (err) {
-        alert("Gagal daftar");
-        console.error(err);
-      }
-    });
+    this.#presenter.init();
   }
 }
