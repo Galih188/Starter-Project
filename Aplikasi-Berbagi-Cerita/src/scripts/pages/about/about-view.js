e02b42d9368b1d2192db9af01586ba71f14a10c0
Galihhh <127461042+Galih188@users.noreply.github.com>
2025-05-12 13:11:50 +0700

    Add files via upload

---
+++ Aplikasi-Berbagi-Cerita/src/scripts/pages/about/about-view.js
@@ -0,0 +1,14 @@
+class AboutView {
+  getTemplate() {
+    return `
+        <section class="container">
+          <h1>Tentang Aplikasi</h1>
+          <br>
+          <p>Aplikasi ini dibuat sebagai proyek submission awal dan akhir untuk menyelesaikan kelas Belajar Pengembangan Web Intermediate.</p>
+          <p>Dikembangkan oleh <strong>Galih Sabilah Rosat</strong>.</p>
+        </section>
+      `;
+  }
+}
+
+export default AboutView;
